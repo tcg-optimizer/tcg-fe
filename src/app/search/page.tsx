5855c584b9c3f936d82beaaf7634d5e95efc1f3b
monognuisy <ysmsmart1@gmail.com>
2025-04-11 13:48:43 +0900

    feat: search page layout

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+
 export default function Search() {
-  return <div className="w-full h-full flex flex-col items-center justify-center">
+  return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Search</h1>
+      <div className="flex flex-col items-center justify-center gap-4">
+        <div className="flex items-center gap-2">
+          <Input className="min-w-[400px]" placeholder="카드 이름을 입력해주세요" />
+          <Button>
+            <SearchIcon className="w-4 h-4" />
+          </Button>
+        </div>
+        <div className="w-full flex justify-end items-center-safe space-x-2">
+          <Checkbox id="check-used" />
+          <label
+            htmlFor="check-used"
+            className="text-sm font-medium leading-tone peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            중고 상품을 포함합니다
+          </label>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
