a625f83787dcce2bc6ee4c9e6e1e7059acfbbad6
monognuisy <ysmsmart1@gmail.com>
2025-05-20 13:52:17 +0900

    feat: add commit hash info in footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import localFont from 'next/font/local';
 import StoreInitializer from '@/components/StoreInitializer';
-import Link from 'next/link';
-import Image from 'next/image';
-import { MailIcon } from 'lucide-react';
+import Footer from '@/components/Footer';
 
 const pretendard = localFont({
   src: '../../public/fonts/PretendardJPVariable.woff2',
@@ -30,37 +28,7 @@ export default function RootLayout({
         <StoreInitializer />
         <div className="mx-auto">
           <main>{children}</main>
-          <footer className="py-6 text-center text-gray-500 text-sm flex gap-4 w-full justify-center items-center">
-            <p className="border-r-2 pr-4">
-              © 2025 tcgscanner. All rights reserved.
-            </p>
-            <p className="border-r-2 pr-4">
-              <Link
-                href="https://github.com/orgs/tcg-optimizer/repositories"
-                target="_blank"
-                className="cursor-pointer flex items-center"
-              >
-                <Image
-                  src="/icons/github-mark.svg"
-                  alt="github"
-                  width={14}
-                  height={14}
-                  className="inline-block"
-                />
-                <label className="ml-2 cursor-pointer">Page Source</label>
-              </Link>
-            </p>
-            <p>
-              <Link
-                href="mailto:tcgscanner.site@gmail.com"
-                target="_blank"
-                className="flex items-center cursor-pointer"
-              >
-                <MailIcon className="w-4 h-4" />
-                <label className="ml-2 cursor-pointer">Contact</label>
-              </Link>
-            </p>
-          </footer>
+          <Footer />
         </div>
       </body>
     </html>
---
+++ src/components/Footer.tsx
@@ -0,0 +1,50 @@
+import { MailIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className="py-6 px-6 text-center text-gray-500 text-sm grid grid-cols-[1fr_auto_1fr] gap-4 w-full justify-center items-center mt-8">
+      <div></div>
+      <div className="flex gap-4 justify-center">
+        <p className="border-r-2 pr-4">
+          © 2025 tcgscanner. All rights reserved.
+        </p>
+        <p className="border-r-2 pr-4">
+          <Link
+            href="https://github.com/orgs/tcg-optimizer/repositories"
+            target="_blank"
+            className="cursor-pointer flex items-center"
+          >
+            <Image
+              src="/icons/github-mark.svg"
+              alt="github"
+              width={14}
+              height={14}
+              className="inline-block"
+            />
+            <label className="ml-2 cursor-pointer">Page Source</label>
+          </Link>
+        </p>
+        <p>
+          <Link
+            href="mailto:tcgscanner.site@gmail.com"
+            target="_blank"
+            className="flex items-center cursor-pointer"
+          >
+            <MailIcon className="w-4 h-4" />
+            <label className="ml-2 cursor-pointer">Contact</label>
+          </Link>
+        </p>
+      </div>
+      <div>
+        <p className="text-xs ml-auto w-fit text-transparent" aria-hidden>
+          {`${process.env.FRONT_VERSION ?? 'dev'}`} /{' '}
+          {`${process.env.BACK_VERSION ?? 'dev'}`}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
