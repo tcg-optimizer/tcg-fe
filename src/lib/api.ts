42165c1f7d3a8e41fd34fed3664632c9c55349cb
monognuisy <ysmsmart1@gmail.com>
2025-11-16 16:22:17 +0900

    hotfix: api 주소 수정

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,6 @@
 // 내부 API 라우트 URL 생성 함수
 export function getBaseUrl() {
-  return process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}`
-    : process.env.NEXT_PUBLIC_API_URL || '';
+  return process.env.NEXT_PUBLIC_API_URL || '';
 }
 
 // 내부 API 라우트 URL
